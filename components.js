de28dfebfbcb0feca4f123d64c01c1d07050aaec
Carlos León <cleon@ucm.es>
2025-02-04 12:58:58 +0100

    components con sintaxis nueva

--- components.js
+++ components.js
@@ -1,15 +1,12 @@
-// By Carlos León, 2016
+// By Carlos León, 2025
 // Licensed under Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
-
-'use strict';
-
 //////////////////////////////////////////////////////////////////////////////
 
 // Entity type to differentiate entities and have them attack those not
 // belonging to the same kind
-var EntityType = {
-    GOOD: 0,
-    EVIL: 1
+const EntityType = {
+  GOOD: 0,
+  EVIL: 1
 };
 
 // Entity constructor
@@ -17,197 +14,205 @@ var EntityType = {
 // Entities have a name (it doesn't have to be unique, but it helps) and a type
 //
 // Additionally, entities accept a list of instantiated components
-function Entity(entityName, entityType, components) {
-    var self = this;
+class Entity {
+  constructor(entityName, entityType, components) {
     this.entityName = entityName;
 
     // Instead of assigning the parameter, we call `addComponent`, which is a
     // bit smarter than default assignment
     this.components = [];
-    components.forEach(function(component) {
-        self.addComponent(component);
-    });
+    components.forEach((component) => { this.addComponent(component); });
     this.type = entityType;
-}
+  }
 
-Entity.prototype.addComponent = function(component) {
+  addComponent(component) {
     this.components.push(component);
     component.entity = this;
-};
-
-// This function delegates the tick on the components, gathering their messages
-// and aggregating them into a single list of messages to be delivered by the
-// message manager (the game itself in this case
-Entity.prototype.tick = function() {
-    var outcoming = [];
-    this.components.forEach(function(component) {
-        var messages = component.tick();
-        messages.forEach(function (message) {
-            outcoming.push(message);
-        });
+  }
+  // This function delegates the tick on the components, gathering their messages
+  // and aggregating them into a single list of messages to be delivered by the
+  // message manager (the game itself in this case
+  tick() {
+    const outcoming = [];
+    this.components.forEach((component) => {
+      const messages = component.tick();
+      messages.forEach((message) => {
+        outcoming.push(message);
+      });
     });
     return outcoming;
-};
+  };
 
-// All received messages are forwarded to the components
-Entity.prototype.receive = function(message) {
+  // All received messages are forwarded to the components
+  receive(message) {
     // If the receiver is `null`, this is a broadcast message that must be
     // accepted by all entities
-    if(!message.receiver || message.receiver === this) {
-        this.components.forEach(function(component) {
-            component.receive(message);
-        });
+    if (!message.receiver || message.receiver === this) {
+      this.components.forEach((component) => {
+        component.receive(message);
+      });
     }
-};
+  };
+}
+
+
+
 //////////////////////////////////////////////////////////////////////////////
 // if the receiver is null, it is a broadcast message
-function Message(receiver) {
+class Message {
+  constructor(receiver) {
     this.receiver = receiver;
+
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////
-function Component(entity) {
+class Component {
+  constructor(entity) {
     this.entity = entity;
     this.messageQueue = [];
-}
+  }
 
-Component.prototype.tick = function() {
+  tick() {
     // We return a copy of the `messageQueue`, and we empty it
-    var aux = this.messageQueue;
+    const aux = [...this.messageQueue];
     this.messageQueue = [];
     return aux;
-};
-Component.prototype.receive = function(message) {
-};
+  };
+  receive(message) {
+  };
+
+}
+
 
 
 //////////////////////////////////////////////////////////////////////////////
 
-function Game(entities) {
+class Game {
+  constructor(entities) {
     this.entities = entities;
     this.messageQueue = [];
-}
+  }
 
-Game.prototype.mainLoop = function (ticks) {
-    var i = 0;
+  mainLoop(ticks) {
+    let i = 0;
     function line() {
-        console.log("-----------------------------------------");
+      console.log("-----------------------------------------");
     }
-    while(!ticks || i < ticks) {
-        line();
-        console.log("Tick number " + i);
-        line();
-        this.tick();
-        i++;
+    while (!ticks || i < ticks) {
+      line();
+      console.log("Tick number " + i);
+      line();
+      this.tick();
+      i++;
     }
-};
+  };
 
-// Each tick, all entities are notified by calling their `tick` function
-Game.prototype.tick = function () {
-    var self = this;
-
+  // Each tick, all entities are notified by calling their `tick` function
+  tick() {
     // We create `Presence` messages for all entities to let others that they
     // exists in the game
-    this.entities.forEach(function(entity) {
-        self.messageQueue.push(new Presence(entity));
+    this.entities.forEach((entity) => {
+      this.messageQueue.push(new Presence(entity));
     });
 
     // All messages coming from the entities are put in the queue
-    this.entities.forEach(function(entity) {
-        var tickMessages = entity.tick();
+    this.entities.forEach((entity) => {
+      let tickMessages = entity.tick();
 
-        tickMessages.forEach(function(tickMessage) {
-            self.messageQueue.push(tickMessage);
-        });
+      tickMessages.forEach((tickMessage) => {
+        this.messageQueue.push(tickMessage);
+      });
     });
 
     this.deliver();
-};
+  };
 
 
-// All messages in the queue are delivered to all the entities
-Game.prototype.deliver = function() {
-    var self = this;
+  // All messages in the queue are delivered to all the entities
+  deliver() {
 
-    this.messageQueue.forEach(function(message) {
-        if(!message.receiver) {         
-            self.entities.forEach(function(entity) {
-                entity.receive(message);
-            });
-        }
-        else {
-            message.receiver.receive(message);
-        }
+
+    this.messageQueue.forEach((message) => {
+      if (!message.receiver) {
+        this.entities.forEach(function(entity) {
+          entity.receive(message);
+        });
+      }
+      else {
+        message.receiver.receive(message);
+      }
     });
 
     this.messageQueue = [];
-};
+  };
+}
 
 //////////////////////////////////////////////////////////////////////////////
 // Components
 //////////////////////////////////////////////////////////////////////////////
-function Attacker(entity) {
-    Component.call(this, entity);
-}
-Attacker.prototype = Object.create(Component.prototype);
-Attacker.prototype.constructor = Attacker;
+class Attacker extends Component {
+  constructor(entity) {
+    super(entity);
+  }
 
-Attacker.prototype.receive = function(message) {
-    if(message instanceof Presence) {
-        if(message.who.type != this.entity.type) {
-            this.messageQueue.push(new Attack(this.entity, message.who));
-        }
+  receive(message) {
+    if (message instanceof Presence) {
+      if (message.who.type != this.entity.type) {
+        this.messageQueue.push(new Attack(this.entity, message.who));
+      }
     }
-};
+  };
+}
 
 //////////////////////////////////////////////////////////////////////////////
-function Defender(entity) {
-    Component.call(this, entity);
-}
-Defender.prototype = Object.create(Component.prototype);
-Defender.prototype.constructor = Defender;
+class Defender extends Component {
+  constructor(entity) {
+    super(entity);
+  }
 
-Defender.prototype.receive = function(message) {
-    if(message instanceof Attack) {
-        console.log(this.entity.entityName + " was attacked by " + message.who.entityName);
+  receive(message) {
+    if (message instanceof Attack) {
+      console.log(this.entity.entityName + " was attacked by " + message.who.entityName);
     }
-};
+  };
+}
 
 
 
 //////////////////////////////////////////////////////////////////////////////
 // Messages
 //////////////////////////////////////////////////////////////////////////////
-function Presence(who, receiver) {
-    Message.call(this, receiver);
+class Presence extends Message {
+  constructor(who, receiver) {
+    super(receiver);
     this.who = who;
+  }
 }
-Presence.prototype = Object.create(Message.prototype);
-Presence.prototype.constructor = Presence;
-//////////////////////////////////////////////////////////////////////////////
-function Attack(who, receiver) {
-    Message.call(this, receiver);
-    this.who = who;
-}
-Attack.prototype = Object.create(Message.prototype);
-Attack.prototype.constructor = Attack;
 
+//////////////////////////////////////////////////////////////////////////////
+class Attack extends Message {
+  constructor(who, receiver) {
+    super(receiver);
+    this.who = who;
+  }
+
+}
 //////////////////////////////////////////////////////////////////////////////
 
 
 
 // helper functions creating new components
-var attacker = function() { return new Attacker(); };
-var defender = function() { return new Defender(); };
+const attacker = () => new Attacker();
+const defender = () => new Defender();
 
 // entities in the game
-var link = new Entity("link", EntityType.GOOD, [attacker(), defender()]);
-var ganon = new Entity("ganon", EntityType.EVIL, [attacker(), defender()]);
-var octorok = new Entity("octorok", EntityType.EVIL, [defender()]);
-var armos = new Entity("armos", EntityType.EVIL, [attacker()]);
+const link = new Entity("link", EntityType.GOOD, [attacker(), defender()]);
+const ganon = new Entity("ganon", EntityType.EVIL, [attacker(), defender()]);
+const octorok = new Entity("octorok", EntityType.EVIL, [defender()]);
+const armos = new Entity("armos", EntityType.EVIL, [attacker()]);
 
 // we create the game with the entities
-var game = new Game([link, ganon, armos, octorok]);
+const game = new Game([link, ganon, armos, octorok]);
 
 game.mainLoop(10);
-
